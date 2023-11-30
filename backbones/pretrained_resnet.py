4bc30d0392815618999b873df1e0ab9a4862e970
Lena Loye <92756585+lenaloye@users.noreply.github.com>
2023-11-30 16:33:54 +0100

    Update the bacbone for reakhis dataset

---
+++ backbones/pretrained_resnet.py
@@ -0,0 +1,30 @@
+from torch import nn as nn
+from torchvision import models
+
+class PreTrainedResNet():
+    def __init__(self, type='ResNet18', flatten = False):
+        super(PreTrainedResNet, self).__init__()
+        if type == 'ResNet18':
+            resnet = models.resnet18(weights = models.ResNet18_Weights.DEFAULT)
+        elif type == 'ResNet34':
+            resnet = models.resnet34(weigths = models.ResNet34_Weights.DEFAULT)
+
+        trunk = [*resnet.children()][:-2]
+        
+        if flatten:
+            trunk.append(nn.Flatten())
+            self.final_feat_dim = 512*7*7
+        else:
+            self.final_feat_dim = [512, 7, 7]
+
+        self.model = nn.Sequential(*trunk)
+        
+    def forward(self, x):
+        out = self.model(x)
+        return out
+
+def ResNet18(x_dim, flatten=True):
+    return PreTrainedResNet('ResNet18', flatten)
+
+def ResNet34(x_dim, flatten=True):
+    return PreTrainedResNet('ResNet34', flatten)
