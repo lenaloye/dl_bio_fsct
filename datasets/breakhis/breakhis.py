663db8f99242402b48d4451f4affadbb0a526d53
treil <leonard.treil@gmail.com>
2023-11-30 14:59:56 +0100

    Making sure that image are of the right size

--- datasets/breakhis/breakhis.py
+++ datasets/breakhis/breakhis.py
@@ -46,7 +46,7 @@ class BHSimpleDataset(BHDataset):
         self.batch_size = batch_size
         self.root = root
         super().__init__()
-        self.transforms = transforms.Compose([transforms.ToTensor()])
+        self.transforms = transforms.Compose([transforms.Resize((460, 700)), transforms.ToTensor()])
 
     def __getitem__(self, i):
         img_path, label = os.path.join(self.root, self._dataset_name, 'BreaKHis_v1', self.samples[i]), int(self.targets[i])
@@ -83,7 +83,7 @@ class BHSetDataset(BHDataset):
         samples_all, targets_all = self.load_breakhis(mode)
         self.categories = np.unique(targets_all)  # Unique types labels
 
-        self.transforms = transforms.Compose([transforms.ToTensor()])
+        self.transforms = transforms.Compose([transforms.Resize((460, 700)), transforms.ToTensor()])
 
         img_path = os.path.join(self.root, self._dataset_name, 'BreaKHis_v1', samples_all[0])
         img = Image.open(img_path).convert('RGB')
