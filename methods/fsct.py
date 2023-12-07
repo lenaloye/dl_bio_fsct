c02855c522ae1e745459fb5ba12a4136fa4bbb1c
treil <leonard.treil@gmail.com>
2023-12-07 14:53:33 +0100

    Update fsct.py

--- methods/fsct.py
+++ methods/fsct.py
@@ -16,7 +16,7 @@ class FSCT(MetaTemplate):
         self.ATTN = Attention(dim)
         self.sm = nn.Softmax(dim = -2)
         self.proto_weight = nn.Parameter(torch.ones(n_way, n_support, 1))
-		
+        
         self.FFN = nn.Sequential(nn.LayerNorm(dim),
                                  nn.Linear(dim, 512),
                                  nn.GELU(),
@@ -25,7 +25,7 @@ class FSCT(MetaTemplate):
         self.linear = nn.Sequential(nn.LayerNorm(dim),
                                     nn.Linear(dim, 64),
                                     CosineDistLinear(64, 1))
-		
+
 
     def set_forward(self, x, is_feature=False):
         z_support, z_query = self.parse_feature(x, is_feature)
@@ -33,12 +33,12 @@ class FSCT(MetaTemplate):
         z_support = z_support.contiguous()
         z_proto = (z_support.view(self.n_way, self.n_support, -1)* self.sm(self.proto_weight)).sum(1).unsqueeze(0)  # the shape of z is [1, n_data, n_dim]
         z_query = z_query.contiguous().view(self.n_way * self.n_query, -1).unsqueeze(1)
-
+        
         x = self.ATTN(q = z_proto, k = z_query, v = z_query) + z_proto
         x = self.FFN(x) + x
         
         scores = self.linear(x).squeeze()
-		
+
         return scores
 
     def set_forward_loss(self, x):
@@ -48,7 +48,7 @@ class FSCT(MetaTemplate):
         scores = self.set_forward(x)
 
         return self.loss_fn(scores, y_query )
-		
+
 
 class Attention(nn.Module):
     def __init__(self, dim):
@@ -73,10 +73,10 @@ class Attention(nn.Module):
         
         out = torch.matmul(dots, f_v)                      # (h, q, n, d_h)
         out = out.permute(1, 2, 0, 3).contiguous()
-		out = out.view(out.shape[0], out.shape[1], -1)  # (q, n, d)
+        out = out.view(out.shape[0], out.shape[1], -1)  # (q, n, d)
         
         return self.output_linear(out)
-		
+
 
 class CosineDistLinear(nn.Module):
     def __init__(self, indim, outdim):
@@ -88,15 +88,13 @@ class CosineDistLinear(nn.Module):
     def forward(self, x):
         x_norm = torch.norm(x, p=2, dim =1).unsqueeze(1).expand_as(x)
         x_normalized = x.div(x_norm + 0.00001)
-		
-		L_norm = torch.norm(self.L.weight.data, p=2, dim =1).unsqueeze(1).expand_as(self.L.weight.data)
-		self.L.weight.data = self.L.weight.data.div(L_norm + 0.00001)
-		
+        L_norm = torch.norm(self.L.weight.data, p=2, dim =1).unsqueeze(1).expand_as(self.L.weight.data)
+        self.L.weight.data = self.L.weight.data.div(L_norm + 0.00001)
         cos_dist = self.L(x_normalized) 
         scores = self.scale_factor * (cos_dist)
 
-        return scores	
-	
+        return scores
+
 def cosine_distance(x1, x2):
     '''
     x1      =  [b, h, n, k]
