1dcf15c0a811011bbf2bc1d4a8090e2e3f51794d
Lena Loye <92756585+lenaloye@users.noreply.github.com>
2023-12-08 13:10:32 +0100

    New bash file

---
+++ run_breakhis.sh
@@ -0,0 +1,74 @@
+### RESNET18
+
+# with FSCT
+#python run.py exp.name=breakhis_fsct_ResNet18 method=fsct dataset=breakhis
+#python run.py exp.name=breakhis_fsct_ResNet18 method=fsct dataset=breakhis_x40
+#python run.py exp.name=breakhis_fsct_ResNet18 method=fsct dataset=breakhis_x100
+#python run.py exp.name=breakhis_fsct_ResNet18 method=fsct dataset=breakhis_x200
+#python run.py exp.name=breakhis_fsct_ResNet18 method=fsct dataset=breakhis_x400
+
+# with MatchingNet
+#python run.py exp.name=breakhis_matchingnet_ResNet18 method=matchingnet dataset=breakhis
+#python run.py exp.name=breakhis_matchingnet_ResNet18 method=matchingnet dataset=breakhis_x40
+#python run.py exp.name=breakhis_matchingnet_ResNet18 method=matchingnet dataset=breakhis_x100
+#python run.py exp.name=breakhis_matchingnet_ResNet18 method=matchingnet dataset=breakhis_x200
+#python run.py exp.name=breakhis_matchingnet_ResNet18 method=matchingnet dataset=breakhis_x400
+
+# with Baseline
+#python run.py exp.name=breakhis_baseline_ResNet18 method=baseline dataset=breakhis
+#python run.py exp.name=breakhis_baseline_ResNet18 method=baseline dataset=breakhis_x40
+#python run.py exp.name=breakhis_baseline_ResNet18 method=baseline dataset=breakhis_x100
+#python run.py exp.name=breakhis_baseline_ResNet18 method=baseline dataset=breakhis_x200
+#python run.py exp.name=breakhis_baseline_ResNet18 method=baseline dataset=breakhis_x400
+
+# with MAML
+#python run.py exp.name=breakhis_maml_ResNet18 method=maml dataset=breakhis
+#python run.py exp.name=breakhis_maml_ResNet18 method=maml dataset=breakhis_x40
+#python run.py exp.name=breakhis_maml_ResNet18 method=maml dataset=breakhis_x100
+#python run.py exp.name=breakhis_maml_ResNet18 method=maml dataset=breakhis_x200
+#python run.py exp.name=breakhis_maml_ResNet18 method=maml dataset=breakhis_x400
+
+# with Protonet
+#python run.py exp.name=breakhis_protonet_ResNet18 method=protonet dataset=breakhis
+#python run.py exp.name=breakhis_protonet_ResNet18 method=protonet dataset=breakhis_x40
+#python run.py exp.name=breakhis_protonet_ResNet18 method=protonet dataset=breakhis_x100
+#python run.py exp.name=breakhis_protonet_ResNet18 method=protonet dataset=breakhis_x200
+#python run.py exp.name=breakhis_protonet_ResNet18 method=protonet dataset=breakhis_x400
+
+
+### RESNET34
+
+# with FSCT
+#python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis
+#python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x40
+#python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x100
+#python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x200
+#python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x400
+
+# with MatchingNet
+#python run.py exp.name=breakhis_matchingnet_ResNet34 method=matchingnet dataset=breakhis
+#python run.py exp.name=breakhis_matchingnet_ResNet34 method=matchingnet dataset=breakhis_x40
+#python run.py exp.name=breakhis_matchingnet_ResNet34 method=matchingnet dataset=breakhis_x100
+#python run.py exp.name=breakhis_matchingnet_ResNet34 method=matchingnet dataset=breakhis_x200
+#python run.py exp.name=breakhis_matchingnet_ResNet34 method=matchingnet dataset=breakhis_x400
+
+# with Baseline
+#python run.py exp.name=breakhis_baseline_ResNet34 method=baseline dataset=breakhis
+#python run.py exp.name=breakhis_baseline_ResNet34 method=baseline dataset=breakhis_x40
+#python run.py exp.name=breakhis_baseline_ResNet34 method=baseline dataset=breakhis_x100
+#python run.py exp.name=breakhis_baseline_ResNet34 method=baseline dataset=breakhis_x200
+#python run.py exp.name=breakhis_baseline_ResNet34 method=baseline dataset=breakhis_x400
+
+# with MAML
+#python run.py exp.name=breakhis_maml_ResNet34 method=maml dataset=breakhis
+#python run.py exp.name=breakhis_maml_ResNet34 method=maml dataset=breakhis_x40
+#python run.py exp.name=breakhis_maml_ResNet34 method=maml dataset=breakhis_x100
+#python run.py exp.name=breakhis_maml_ResNet34 method=maml dataset=breakhis_x200
+#python run.py exp.name=breakhis_maml_ResNet34 method=maml dataset=breakhis_x400
+
+# with Protonet
+#python run.py exp.name=breakhis_protonet_ResNet34 method=protonet dataset=breakhis
+#python run.py exp.name=breakhis_protonet_ResNet34 method=protonet dataset=breakhis_x40
+#python run.py exp.name=breakhis_protonet_ResNet34 method=protonet dataset=breakhis_x100
+#python run.py exp.name=breakhis_protonet_ResNet34 method=protonet dataset=breakhis_x200
+#python run.py exp.name=breakhis_protonet_ResNet34 method=protonet dataset=breakhis_x400
