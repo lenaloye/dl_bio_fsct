cddae1060f8f3c01532521d4bb0295dd35dbb162
Lena Loye <92756585+lenaloye@users.noreply.github.com>
2023-12-08 13:12:23 +0100

    Suppress old bash file

--- run_breakhis_all_zooms.sh
+++
@@ -1,4 +0,0 @@
-python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x40
-python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x100
-python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x200
-python run.py exp.name=breakhis_fsct_ResNet34 method=fsct dataset=breakhis_x400
